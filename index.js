ec3676907beb1204664b7c4005779bb7cc652f4f
Anthony Babka <anthonybabka@gmail.com>
2022-02-13 10:18:54 -0800

    fixed styling issues

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+window.alert("⭐ Instructions Please read first! ⭐ \n Use your mouse to choose which answer you think is correct. \n After your selection you'll move to the next question. \n At the end you'll see your score! \n \n Good Luck! 🍀")
+
 function left(){
     document.getElementById("left").innerHTML="test"
 }
