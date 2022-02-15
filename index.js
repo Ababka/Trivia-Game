a181783e76ea907a8f42a34f63b9cadc0da07212
Anthony Babka <anthonybabka@gmail.com>
2022-02-15 14:09:50 -0800

    check point

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-window.alert("⭐ Instructions Please read first! ⭐ \n Use your mouse to choose which answer you think is correct. \n After your selection you'll move to the next question. \n At the end you'll see your score! \n \n Good Luck! 🍀")
+// window.alert("⭐ Instructions Please read first! ⭐ \n Use your mouse to choose which answer you think is correct. \n After your selection you'll move to the next question. \n At the end you'll see your score! \n \n Good Luck! 🍀")
 
-function left(){
-    document.getElementById("left").innerHTML="test"
-}
+const questionContainer = document.getElementById("question");
+const submitButton = document.getElementById("left", "right");
+const myQuestions = [{
+    question: "Test Question 1",
+    answers: {
+        left: "True",
+        right: "False"
+        },
+    correctAnswer: "right",
 
-function right(){
-    document.getElementById("right").innerHTML="test"
-}
+    question: "Test Question 2",
+    answers: {
+        left: "True",
+        right: "False"
+        },
+    correctAnswer: "left"
+}];
