342428218ad3f9ea6865b18def5b8271d846e0e3
Anthony Babka <anthonybabka@gmail.com>
2022-02-15 21:07:17 -0800

    adding functions

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 // window.alert("⭐ Instructions Please read first! ⭐ \n Use your mouse to choose which answer you think is correct. \n After your selection you'll move to the next question. \n At the end you'll see your score! \n \n Good Luck! 🍀")
 
-const questionContainer = document.getElementById("question");
-const submitButton = document.getElementById("left", "right");
-const myQuestions = [{
+const questionContainer = document.getElementById("question")
+const trueButton = document.getElementById("left")
+const falseButton = document.getElementById("right")
+// const name = promt ("Enter a username!")
+var score = 0
+var index = 0
+var myQuestions = [{
     question: "Test Question 1",
     answers: {
         left: "True",
         right: "False"
         },
-    correctAnswer: "right",
-
+    correctAnswer: "right", //false
+    },{
     question: "Test Question 2",
     answers: {
         left: "True",
         right: "False"
         },
-    correctAnswer: "left"
-}];
+    correctAnswer: "left" //true
+    },{
+    question: "Test Question 2",
+    answers: {
+        left: "True",
+        right: "False"
+        },
+    correctAnswer: "right", //false
+    },{
+    question: "Test Question 2",
+    answers: {
+        left: "True",
+        right: "False"
+        },
+    correctAnswer: "right", //false
+}]
+
+trueButton.addEventListener("click", function(){
+    var correctAnswer = "left"
+    if(correctAnswer === myQuestions[index].correctAnswer){
+        score++
+    }
+    index++
+    questionContainer.innerHTML=`<img src="Questions/test${index}.png" alt="Question ${index}">`
+    console.log(score)
+})
+
+falseButton.addEventListener("click", function(){
+    var correctAnswer = "right"
+    if(correctAnswer === myQuestions[index].correctAnswer){
+        score++
+    }
+    index++
+    questionContainer.innerHTML=`<img src="Questions/test${index}.png" alt="Question ${index}">`
+    console.log(score)
+})
+
+
+//display score
